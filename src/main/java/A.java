cb7ce410b8345e4cfd920e6d8fed7be5a9a22854
skf001 <test@skf001.com>
2017-10-30 18:10:53 +0800

    在测试一次

--- src/main/java/A.java
+++ src/main/java/A.java
@@ -2,7 +2,8 @@
  * Created by skf on 2017/10/26.
  */
 public class A {
-    public static void main(String[] args) {
+    public static void main(String [] args) {
+        System.out.println("haha");
         System.out.println("hello github!");
     }
 }
