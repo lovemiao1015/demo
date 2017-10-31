26db3d66b9c51ce1123ab1e1e035b6bb73ab5dfd
skf001 <test@skf001.com>
2017-10-31 09:21:23 +0800

    Merge branch 'master' of https://github.com/lovemiao1015/demo
    # Conflicts:
    #	src/main/java/A.java

--- src/main/java/A.java
+++ src/main/java/A.java
@@ -3,8 +3,7 @@
  */
 public class A {
     public static void main(String [] args) {
-        System.out.println("haha");
-        System.out.println("heihei");
+        System.out.println("我是分支一号");
         System.out.println("hello github!");
     }
 }
